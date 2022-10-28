674b35744d6c830f8df56911a1446c90387ce7d8
el-coreano <116873588+el-coreano@users.noreply.github.com>
2022-10-29 01:30:04 +0900

    Routing 학습 및 page 추가 (#1)
    * refactor script
    
    * Feat: learn routing and add simple pages

---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+import PageLayout from '../components/layout/PageLayout';
+
+export default function About() {
+	return <PageLayout metaData={{ title: 'about' }}>about</PageLayout>;
+}
---
+++ pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import PageLayout from '../../components/layout/PageLayout';
+
+const BlogMain = () => {
+	return (
+		<PageLayout metaData={{ title: 'blog main' }}>
+			<h1>blog main</h1>
+		</PageLayout>
+	);
+};
+
+export default BlogMain;
